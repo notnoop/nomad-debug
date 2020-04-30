f1d80723143e214096cb8c58d0dc190959c5eea2
Mahmood Ali <mahmood@hashicorp.com>
2020-04-30 07:57:00 -0400

    JobBatchDeregisterRequest is special

--- raft_logs.go
+++ raft_logs.go
@@ -137,15 +137,46 @@ func decode(e *raft.Log) (*logMessage, error) {
 
 	if len(data) != 0 {
 		decoder := codec.NewDecoder(bytes.NewReader(data), MsgpackHandle)
+
 		var v interface{}
-		if err := decoder.Decode(&v); err != nil {
-			return nil, fmt.Errorf("failed to decode body %v", err)
+		var err error
+		if m.CommandType == msgTypeNames[structs.JobBatchDeregisterRequestType] {
+			var vr structs.JobBatchDeregisterRequest
+			err = decoder.Decode(&vr)
+			v = jsonifyJobBatchDeregisterRequest(&vr)
+		} else {
+			var vr interface{}
+			err = decoder.Decode(&vr)
+			v = vr
+		}
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to decode log entry at index %d: failed to decode body of %v.%v %v\n", e.Index, e.Type, m.CommandType, err)
+			v = "FAILED TO DECODE DATA"
 		}
 		fixTime(v)
 		m.Body = v
 	}
 
 	return m, nil
+}
+
+func jsonifyJobBatchDeregisterRequest(v *structs.JobBatchDeregisterRequest) interface{} {
+	var data struct {
+		Jobs  map[string]*structs.JobDeregisterOptions
+		Evals []*structs.Evaluation
+		structs.WriteRequest
+	}
+	data.Evals = v.Evals
+	data.WriteRequest = v.WriteRequest
+
+	data.Jobs = make(map[string]*structs.JobDeregisterOptions, len(v.Jobs))
+	if len(v.Jobs) != 0 {
+		for k, v := range v.Jobs {
+			data.Jobs[k.Namespace+"."+k.ID] = v
+		}
+	}
+	return data
 }
 
 var logTypes = map[raft.LogType]string{
